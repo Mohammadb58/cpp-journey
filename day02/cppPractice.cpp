a55f3b4f39389f8df151a9452a39b94c7e946ff6
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-12 09:48:55 -0500

    update

---
+++ day02/cppPractice.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+// This allows us skip writing std for string and cout in this case 
+using namespace std;
+
+int main(){
+    const string NAME = "Mohammad";
+    cout << NAME << endl;
+
+    return 0;
+}
--- day02/printInCpp.cpp
+++ day02/printInCpp.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+
 using namespace std;
+
 int main(){
     cout << "Hello, Mohammad\n";
     return 0;
