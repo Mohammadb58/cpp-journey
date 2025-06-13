ce6d7c69a3629ce676d2303d7c23f7c5332df1ad
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-12 21:22:19 -0500

    update

---
+++ day02/variables .cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // int 
+    int year;
+    year = 2025;
+
+    // double 
+    double gpa = 3.1;
+
+    // char
+    char grade = 'A';
+
+    //string 
+    const string NAME = "Mohamad";
+
+    // NAME = 'Ali'
+    grade = 'B';
+    // printing them all
+
+    cout << year << endl;
+    cout << gpa<< endl;
+    cout << grade<< endl;
+    cout << NAME<< endl;
+    return 0;
+}
