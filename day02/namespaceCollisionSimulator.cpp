03c927f86e6c25ae75b766e344f87b7e30e9afa5
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-12 22:23:40 -0500

    update

---
+++ day02/namespaceCollisionSimulator.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+namespace SS{
+    int x = 5;
+}
+int main(){
+    using namespace SS;
+
+    // This is like saying go to SS get x and print it!
+    cout << SS::x << endl;
+
+    // This is a string '55' not the int 55!
+    string x = "55";
+
+    cout << x << endl;
+
+    cout << "THe value of the int x is " << SS::x << " and the value of the string x is " << x << endl;
+
+    // The guessed x 
+    cout << "The guessed x " << x << endl;
+    cout << "The guessed x " << x << endl;
+
+    return 0;
+}
