32d75405c1b685e946b55f03e9716eab7f0f0ff1
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-12 21:58:29 -0500

    update

---
+++ day02/constantPressure.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // PI rev 6 digits 
+    const double PI = 9.51413;
+
+    // Attempting to fix PI misake 
+    // PI = 3.14159;
+    // read-only error 
+
+    // prints 1 
+    // bool calculateTheArea = true ;
+    // prints 0
+    // bool calculateTheArea = false ;
+    // 'True' wont work as a bool
+    //bool calculateTheArea = True ;
+    // true false 0 and 1 work as bool
+     bool calculateTheArea = 1 ;
+    // This retunred 1!
+    //bool calculateTheArea = 2 ;
+
+    cout << calculateTheArea;
+    return 0;
+}
