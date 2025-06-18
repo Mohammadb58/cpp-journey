f576436abb16b8155d6bf3903cd3b03a83926053
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-18 17:01:39 -0500

    update

---
+++ day05/characterIdentityScanner.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char userChar;
+    cout << "\nPlease enter any any single character (letter, digit, or symbol): ";
+    cin >> userChar;
+    /*
+    char newUserChar = (char)userChar;
+    cout << "DEBUG " << userChar << endl;
+    cout << "DEBUG " << newUserChar << endl;
+    */
+    if (userChar >= 48 && userChar <= 57){
+        cout << "\nYou entered " << userChar << " Which is a digit.\n";
+    }
+    else if  (userChar >= 65 && userChar <= 90){
+        cout << "\nYou entered " << userChar << " Which is a capital letter.\n";
+    }
+    else if (userChar >= 97 && userChar <= 122){
+        cout << "\nYou entered " << userChar << " Which is a lowecase letter.\n";
+    }
+    else{
+        cout << "\nIt's something else\n";
+    }
+    return 0;
+}
