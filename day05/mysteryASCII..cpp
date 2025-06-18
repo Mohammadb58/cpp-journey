61b7f966ebc791a4377273d2fabf31046644f0af
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-18 16:18:47 -0500

    update

--- day05/mysteryASCII..cpp
+++ day05/mysteryASCII..cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 int main(){
+    int a;
+    cout << "\nWelcome to the Mystery ASCII program.\n";
+    cout << "\nPlease enter a num from 65 to 90: ";
+    cin >> a;
+    if (a >= 65 && a <=90){
+        char numToChar = (char)a;
+        cout << "\nThe ASCII letter of your num is " << numToChar << "\n\n";
+    }
+    else {
+        cout << "\nInvaild num.\n";
+    }
 
     return 0;
 }
