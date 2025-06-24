0d1d8333ae916d72ef6ed7937fe7c7bd90f4ef0e
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-23 22:04:09 -0500

    update

---
+++ day10/sentinel-ControlledSum&Average.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main(){
+    cout << "------------------------------------------\n";
+    string input;
+    int counter = 0, len, num=0;
+    double sum = 0, count = 0, avr = 0;
+    bool numIsDigtit = false;
+    cout << "\nPlease enter a postive number: ";
+    getline(cin >> ws, input);
+
+    len = input.length();
+    while(bool dig = isdigit(input[counter])){
+        counter++;
+    };
+    if (counter == len){
+        numIsDigtit = true;
+    }
+    
+    if (numIsDigtit == true){ 
+        int num = stoi(input);
+        sum += num;
+        count++;
+        while(num >= 0 && numIsDigtit){
+            cout << "\n\nPlease enter a postive number: ";
+            getline(cin >> ws, input);
+            len = input.length();
+            counter = 0;
+            numIsDigtit = false;
+            while(bool dig = isdigit(input[counter])){
+                counter++;
+            };
+            if (counter == len){
+                numIsDigtit = true;
+                num = stoi(input);
+                sum += num;
+                count++;
+            }
+        }
+        cout << "\n\nInvaild!\n";
+    }
+    else{
+       cout << "\nInvaild!\n"; 
+    }
+    if(count != 0){
+        avr = sum / count;
+    }
+    cout << "\n- Sum: " << sum << '\n';
+    cout << "\n- Count " << count << '\n';
+    cout << "\n- Avrage: " << avr << '\n';
+    
+    cout << "\n------------------------------------------\n";
+    return 0;
+}
