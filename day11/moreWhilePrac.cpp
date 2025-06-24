8a5b562d42ada7215dac8064d4701e2b280f28e8
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-24 09:30:17 -0500

    update

---
+++ day11/moreWhilePrac.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main(){
+    cout << "------------------------------------------\n";
+    string name = "";
+    int i = 0;
+    bool alpha = false;
+    while (i < name.length()){
+        alpha = isalpha(name[i]);
+        if(alpha){
+            cout << '\n' << i << " is " << boolalpha << alpha << '\n';
+            alpha = true;
+        }
+        else{
+            cout << '\n' << i << " is " << boolalpha << alpha << '\n';
+            alpha = false;
+            break;
+        }
+        i++;
+    }
+
+    if(alpha){
+        cout << "\nThe string is all alpha!\n";
+    }
+    else{
+        cout << "\nThe string is not all alpha!\n";
+    }
+
+    cout << "\n------------------------------------------\n";
+    return 0;
+}
