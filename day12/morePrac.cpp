ac243bb4a4b18d1c0ac00f1639428d7a7d6bae90
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-06-30 20:50:46 -0500

    update

---
+++ day12/morePrac.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "\n------------------------------\n";
+    double num;
+    cout << "\nPlease enter a postive none zero num: ";
+    cin >> num;
+
+    while (num <= 0){
+        cout << "\nInvaild num.";
+        cout << "\n\nPlease enter a postive none zero num: ";
+        cin >> num;
+    }
+
+    while (num > 1){
+        cout << '\n' << num;
+        num -= 1;
+    }
+    cout << '\n' << 1;
+    cout << "\n------------------------------\n";
+    return 0;
+}
