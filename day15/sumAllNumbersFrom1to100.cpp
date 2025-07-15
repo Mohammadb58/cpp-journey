7cebf47bb5d11349f82d832d1743e163dc75a54c
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-07-15 18:47:12 -0500

    update

---
+++ day15/sumAllNumbersFrom1to100.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int z = 0;
+    for(int i = 1; i <= 100; i++){
+        z += i;
+    }
+    cout << '\n' << z << '\n';
+
+    return 0;
+}
