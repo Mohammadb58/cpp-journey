76c6ab632f085feffa2c857cef28f5410b47c961
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-07-29 18:23:44 -0500

    update

---
+++ day19/perfectSquare.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "-----------------------------------";
+    for (int i = 1; i <= 20; i++){
+        int z = i * i;
+        cout << z << " ";
+        //z = 0;
+    }
+
+    cout << "\n-----------------------------------";
+    return 0;
+}
