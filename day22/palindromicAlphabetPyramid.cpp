e2b2a55ed2d18432aa917549936a4cad75d83637
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-08-02 21:26:50 -0500

    update

---
+++ day22/palindromicAlphabetPyramid.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "-----------------------------------";
+    int n = 4, num = 0;
+    char convChar = 'A';
+    for (int i = 1; i <= n; i++){
+        for (int j = n*2-1; j > i*2-1; j--){
+            cout << " ";
+        }
+        num = i+64;
+        for (int k = 1; k <= i*2-1; k++){
+            cout << char(num) << " ";
+            if (k <= i){
+                num--;
+            }
+            else{
+                num++;
+            }
+        }
+        cout << endl;
+    }
+    
+    cout << "-----------------------------------";
+    return 0;
+}
+
