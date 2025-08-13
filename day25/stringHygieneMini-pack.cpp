046f5ee1786d70160b33ca111dcf68c8fd61daa6
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-08-12 20:59:42 -0500

    update

---
+++ day25/stringHygieneMini-pack.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string trimEdges(string s){
+    int len = 0, leadingChar, trailingChar;
+    string trimmedString = "";
+    len = s.length();
+    bool allSpaces = true;
+    for (int i = 0; i < len; i++){
+        if (s[i] != ' '){
+            allSpaces = false;
+        }
+    }
+
+    if(!allSpaces && (!s.empty() && (s[0] == ' ' || s[len - 1] == ' '))){
+        leadingChar = s.find_first_not_of(' ');
+        trailingChar = s.find_last_not_of(' ');
+        trimmedString = s.substr(leadingChar, trailingChar-leadingChar+1);
+        return trimmedString;
+    }
+    else {
+        return trimmedString = s;
+    }
+}
+
+int main(){
+
+    cout << "\n-----------------------------------";
+    string s = "";
+    cout << "Enter s string: ";
+    // Didn't use >> ws sinsce it pre trims leading space!
+    getline(cin, s);
+    cout << "String:: " << trimEdges(s);
+
+    cout << "\n-----------------------------------";
+
+    return 0;
+}
