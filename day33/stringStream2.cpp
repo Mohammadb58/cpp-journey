9a5b6a78bd344c001ddbde9d831b7e8ff3180eeb
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-09-04 19:48:05 -0500

    update

--- day33/stringStream2.cpp
+++ day33/stringStream2.cpp
@@ -1,16 +1,21 @@
 #include<iostream>
 #include<sstream>
 #include<string>
+#include<cctype>
 using namespace std;
 
 
 int main(){
     cout << "\n------------------------------";
-    string const TEXT = "Data structures are not fun to learn";
+    string const TEXT = "Data, structures are not fun to !!learn!!";
     string word;
     stringstream TEXTStream(TEXT);
     int WordCounter = 0;
     while(TEXTStream >> word){
+        while(ispunct(word.back()) || word.front()){
+            word.pop_back();
+        }
+        cout << "\n" << word;
         WordCounter++;
     }
     cout << "\nThe string has " << WordCounter << " words!";
