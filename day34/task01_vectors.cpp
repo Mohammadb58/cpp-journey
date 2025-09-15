fae14035049df91a4166164654e0e7e02e77d708
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-09-15 14:58:56 -0500

    update

--- day34/task01_vectors.cpp
+++ day34/task01_vectors.cpp
@@ -4,11 +4,20 @@ using namespace std;
 
 int main() {
     vector<int> vecOfNumbers;
-    for(int i = 0; i <= 10; i++){
+    for(int i = 1; i <= 10; i++){
         vecOfNumbers.push_back(i);
     }
     for(int i = 0; i < vecOfNumbers.size(); i++){
         if(i != vecOfNumbers.size() - 1){
+            cout << vecOfNumbers[i] << " ";
+        }
+        else{
+            cout << vecOfNumbers[i];
+        } 
+    }
+    cout << endl;
+    for(int i = vecOfNumbers.size() - 1; i >= 0; i--){
+        if(i != 0){
             cout << vecOfNumbers[i] << " ";
         }
         else{
