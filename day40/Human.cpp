04045a5d9819e1e118a512aed35a9ba4fc700142
Mohammad Abu Rashed <maburashed543210@gmail.com>
2025-10-07 23:26:45 -0500

    update

--- day40/Human.cpp
+++ day40/Human.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 class Human{
-    public:
+    private:
         // Attributes
         string name;
         string job;
         int age = 0;
-
+    public:
         // Methods
         void eat(){
             cout << "eating\n";
@@ -20,34 +20,61 @@ class Human{
         void train(){
             cout << "training\n";
         }
-};
 
+        // Constructor to set default values
+        Human(string name, string job, int age){
+            setterForName(name);
+            setterForJob(job);
+            setterForAge(age);
+        }            
+        // Destructor
+        ~Human(){
+            // nothing to do here since we didnt alloacte anything on the HEAP
+            cout << name << " OBJECT IS DEAD\n";
+        }
+
+        // Getters 
+        string getterForName(){
+            return name;
+        }
+        string getterForJob(){
+            return job;
+        }
+        int getterForAge(){
+            return age;
+        }
+
+        // Setters
+        void setterForName(string name){
+            this->name = name; // the one we passed 
+        }
+        void setterForJob(string job){
+            this->job = job; // the one we passed 
+        }
+        void setterForAge(int age){
+            if(age >= 18 && age <= 50){
+                this->age = age; // the one we passed 
+            } 
+            else{
+                age = 0;
+            }
+        }
+};
 
 
 int main() {
     cout << "\n\n";
-    Human fighter1;
+    Human fighter1("Topuria", "fighter", 28);
 
-    fighter1.name = "Topuria";
-    fighter1.job = "fighter";
-    fighter1.age = 28;
+    Human fighter2("Makhachev", "fighter", 33);
 
-    Human fighter2;
+    cout << fighter1.getterForName() << endl;
+    cout << fighter1.getterForJob() << endl;
+    cout << fighter1.getterForAge() << endl;
 
-    fighter2.name = "Makhachev";
-    fighter2.job = "fighter, but better than Topuria";
-    fighter2.age = 33;
-
-    cout << fighter1.name << " " << fighter1.job << " " << fighter1.age << endl;
-    cout << fighter2.name << " " << fighter2.job << " " << fighter2.age << endl;
-
-    fighter1.eat();
-    fighter1.sleep();
-    fighter1.train();
-
-    fighter2.eat();
-    fighter2.sleep();
-    fighter2.train();
+    cout << fighter2.getterForName() << endl;
+    cout << fighter2.getterForJob() << endl;
+    cout << fighter2.getterForAge() << endl;
 
     cout << "\n\n";
     return 0;
